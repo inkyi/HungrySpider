83a73edf171320a995f2055ff7577a6ff54f4bc1
InkYi <lhkmaple@163.com>
2016-08-30 10:31:44 +0800

    v1.3

--- src/com/inkyi/HungrySpider/Spider.java
+++ src/com/inkyi/HungrySpider/Spider.java
@@ -370,8 +370,4 @@ public class Spider {
 		init();
 		startLoop();
 	}
-	
-	public static void main(String[] args) {
-		Spider.create().thread(10).executes(1000).urlLength(6).bloomFilter(10000000, 4).run();
-	}
 }
